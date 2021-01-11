ffea0885eab0a2f4249da581f6d5ddf5678c8bfb
jeter5274 <jeter5274@gmail.com>
2021-01-11 17:44:13 +0900

    guestbook 추가

---
+++ src/com/javaex/controller/GuestbookController.java
@@ -0,0 +1,79 @@
+package com.javaex.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.javaex.dao.GuestbookDao;
+import com.javaex.util.WebUtil;
+import com.javaex.vo.GuestbookVo;
+
+@WebServlet("/guestbook")
+public class GuestbookController extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String action = request.getParameter("action");
+		System.out.println(action);
+		
+		GuestbookDao gbDao = new GuestbookDao();
+		
+		if("add".equals(action)) {
+			System.out.println("방명록 등록");
+			
+			//파라미터로 GuestbookVo 생성
+			GuestbookVo gbVo = new GuestbookVo(request.getParameter("name"), request.getParameter("pass"), request.getParameter("content"));
+			
+			//Dao로 DB에 insert
+			gbDao.insert(gbVo);
+			
+			//리다이렉트
+			WebUtil.redirect(request, response, "/mysite2/guestbook");
+			
+		}else if ("dform".equals(action)) {
+			System.out.println("삭제 폼");
+			
+			//포워드
+			WebUtil.forword(request, response, "/WEB-INF/views/guestbook/deleteForm.jsp");
+			
+		}else if ("delete".equals(action)) {
+			System.out.println("삭제");
+			
+			//파라미터 값으로 삭제하고 삭제결과를 리턴받음
+			int result = gbDao.delete(Integer.parseInt(request.getParameter("no")), request.getParameter("pass"));
+			
+			if(result == 0) {	
+				//삭제가 실패할 경우 어트리뷰트에 결과 값을 저장
+				request.setAttribute("delResult", result);
+				
+				//포워드
+				WebUtil.forword(request, response, "/WEB-INF/views/guestbook/deleteForm.jsp");	
+			}else {
+			//리다이렉트
+			WebUtil.redirect(request, response, "/mysite2/guestbook");
+			}
+			
+		}else {	//기본페이지로 설정
+			System.out.println("입력 및 방명록 목록");
+			
+			//방명록을 DB로 부터 불러옴
+			List<GuestbookVo> gbList = gbDao.getList();
+			
+			//어트리뷰트에 방명록List 저장
+			request.setAttribute("GuestbookList", gbList);
+			
+			//포워드
+			WebUtil.forword(request, response, "/WEB-INF/views/guestbook/addList.jsp");
+		}
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ src/com/javaex/dao/GuestbookDao.java
@@ -0,0 +1,152 @@
+package com.javaex.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.javaex.vo.GuestbookVo;
+
+public class GuestbookDao {
+
+	private String driver = "oracle.jdbc.driver.OracleDriver";
+	private String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	private String id = "webdb" , pw = "webdb";
+	
+	private Connection conn = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
+	
+	private int count = 0;
+	
+	private void connectDB() {
+		try {
+			// 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName(driver);
+			// 2. Connection 얻어오기
+			conn = DriverManager.getConnection(url, id, pw);
+
+		} catch (ClassNotFoundException e) {
+		    System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+		    System.out.println("error:" + e);
+		} 
+	}
+	
+	private void closeRs() {
+		// 5. 자원정리
+	    try {
+	        if (rs != null) {
+	            rs.close();
+	        }            	
+	    	if (pstmt != null) {
+	        	pstmt.close();
+	        }
+	    	if (conn != null) {
+	        	conn.close();
+	        }
+	    } catch (SQLException e) {
+	    	System.out.println("error:" + e);
+	    }
+	}
+	
+	public List<GuestbookVo> getList() {
+		
+		List<GuestbookVo> GBList = new ArrayList<GuestbookVo>();
+		
+		connectDB();
+		
+		try {		
+			// 3. SQL문 준비 / 바인딩 / 실행
+			String query="";
+			query +=" select	no,";
+			query +="			name,";
+			query +="			password,";
+			query +="			content,";
+			query +="			reg_date";
+			query +=" from guestbook";
+			
+			pstmt = conn.prepareStatement(query);
+			
+			rs = pstmt.executeQuery();
+			
+			// 4.결과처리
+			while(rs.next()) {
+								
+				GuestbookVo gbVo = new GuestbookVo(rs.getInt("no"), rs.getString("name"), rs.getString("password"), rs.getString("content"), rs.getString("reg_date"));
+				
+				GBList.add(gbVo);
+			}
+		} catch (SQLException e) {
+		    System.out.println("error:" + e);
+		}
+		
+		closeRs();
+		
+		return GBList;
+	}
+	
+	public int insert(GuestbookVo gbVo) {
+		
+		connectDB();
+		
+		try {		
+			// 3. SQL문 준비 / 바인딩 / 실행
+			String query="";
+			query +=" insert into guestbook";
+			query +=" values (seq_guestbook_no.nextval, ?, ?, ?, sysdate)";
+			
+			pstmt = conn.prepareStatement(query);
+			
+			pstmt.setString(1, gbVo.getName());
+			pstmt.setString(2, gbVo.getPassword());
+			pstmt.setString(3, gbVo.getContent());
+			
+			count = pstmt.executeUpdate();
+			
+			// 4.결과처리
+			//System.out.println("[DAO] : " +count+ "건 등록");
+			
+		} catch (SQLException e) {
+		    System.out.println("error:" + e);
+		}
+		
+		closeRs();
+		
+		return count;
+	}
+	
+	public int delete(int no, String pwd) {
+		
+		connectDB();
+		
+		try {		
+			// 3. SQL문 준비 / 바인딩 / 실행
+			String query="";
+			query +=" delete guestbook";
+			query +=" where no = ?";
+			query +=" and password = ?";
+			
+			pstmt = conn.prepareStatement(query);
+			
+			pstmt.setInt(1, no);
+			pstmt.setString(2, pwd);
+			
+			count = pstmt.executeUpdate();
+			
+			// 4.결과처리
+			System.out.println("[DAO] : " +count+ "건 삭제");
+			
+		} catch (SQLException e) {
+		    System.out.println("error:" + e);
+		}
+		
+		closeRs();
+		
+		return count;
+		
+	}
+}
